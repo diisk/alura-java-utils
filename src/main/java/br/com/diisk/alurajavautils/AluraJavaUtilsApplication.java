8bfebeaaa198d8475f77cdb5c04192d603d1cf21
DiisK <diisk@hotmail.com>
2024-05-20 20:44:24 -0300

    first commit

---
+++ src/main/java/br/com/diisk/alurajavautils/AluraJavaUtilsApplication.java
@@ -0,0 +1,13 @@
+package br.com.diisk.alurajavautils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraJavaUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraJavaUtilsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/diisk/alurajavautils/AluraJavaUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diisk.alurajavautils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraJavaUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
