af5973d471da5eb4bf7bd89b20ac1da647a46de5
DiisK <diisk@hotmail.com>
2024-05-20 23:16:12 -0300

    .

---
+++ src/main/java/br/com/diisk/alurajavautils/reflect/Transformator.java
@@ -0,0 +1,42 @@
+package br.com.diisk.alurajavautils.reflect;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
+public class Transformator {
+
+    public <T, Z> Z transform(Z input) throws ClassNotFoundException, InstantiationException, IllegalAccessException,
+            IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+        Class<?> source = input.getClass();
+        Class<?> target = Class.forName(source + "DTO");
+
+        Z targetClass = (Z) target.getDeclaredConstructor().newInstance();
+
+        Field[] sourceFields = source.getDeclaredFields();
+        Field[] targetFields = target.getDeclaredFields();
+
+        Arrays.stream(sourceFields)
+                .forEach(sourceField -> Arrays.stream(targetFields)
+                        .forEach(targetField -> {
+                            if (validate(sourceField, targetField)) {
+                                sourceField.setAccessible(true);
+                                targetField.setAccessible(true);
+                                try {
+                                    targetField.set(targetClass, sourceField.get(input));
+                                } catch (IllegalArgumentException | IllegalAccessException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }));
+
+        return targetClass;
+    }
+
+    private Boolean validate(Field sourceField, Field targetField) {
+        return sourceField.getName().equals(targetField.getName())
+                && sourceField.getType().equals(targetField.getType());
+    }
+
+}
